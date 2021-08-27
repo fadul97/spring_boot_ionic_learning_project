a3d3c9c2adb39da8e0b800ddfce841905b66f863
Leonardo Fadul <lsffadul@gmail.com>
2021-08-27 10:16:39 -0300

    Created H2 database and automatic creation of database

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/domain/Category.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/domain/Category.java
@@ -1,11 +1,19 @@
 package com.leonardofadul.springboot.ionic.learning.project.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
     private String name;
 
     public Category(){
