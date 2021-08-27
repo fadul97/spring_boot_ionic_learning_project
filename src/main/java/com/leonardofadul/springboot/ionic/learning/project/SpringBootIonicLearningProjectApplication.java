a7d18525a73b944cdfe7fc7808b702f76324fb7a
Leonardo Fadul <lsffadul@gmail.com>
2021-08-27 08:50:38 -0300

    Project created

---
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/SpringBootIonicLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.leonardofadul.springboot.ionic.learning.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicLearningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardofadul/springboot/ionic/learning/project/SpringBootIonicLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardofadul.springboot.ionic.learning.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIonicLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
