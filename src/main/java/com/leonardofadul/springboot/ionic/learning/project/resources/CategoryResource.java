099470f265054d442e6154def0781078b03a2e2c
Leonardo Fadul <lsffadul@gmail.com>
2021-08-27 09:40:05 -0300

    Tested first domain class - Category

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/resources/CategoryResource.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/resources/CategoryResource.java
@@ -1,18 +1,27 @@
 package com.leonardofadul.springboot.ionic.learning.project.resources;
 
+import com.leonardofadul.springboot.ionic.learning.project.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-//    private Integer id;
-//    private String abc;
-
     @RequestMapping(method = RequestMethod.GET)
-    public String list(){
-        return "Rest is working!";
+    public List<Category> list(){
+
+        Category cat1 = new Category(1, "Informatics");
+        Category cat2 = new Category(2, "Office");
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(cat1);
+        categoryList.add(cat2);
+
+        return categoryList;
     }
 }
