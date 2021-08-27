dcbeaeec5a16e70a02335ecc38ca0cbba9361297
Leonardo Fadul <lsffadul@gmail.com>
2021-08-27 11:09:06 -0300

    Created instantiation operation

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/SpringBootIonicLearningProjectApplication.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/SpringBootIonicLearningProjectApplication.java
@@ -1,13 +1,29 @@
 package com.leonardofadul.springboot.ionic.learning.project;
 
+import com.leonardofadul.springboot.ionic.learning.project.domain.Category;
+import com.leonardofadul.springboot.ionic.learning.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootIonicLearningProjectApplication {
+public class SpringBootIonicLearningProjectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIonicLearningProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informatics");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
