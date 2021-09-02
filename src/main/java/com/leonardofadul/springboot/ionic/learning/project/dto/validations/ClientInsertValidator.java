3e4ba27861433ef6ae120ce4ea4daab6595d3ea0
Leonardo Fadul <lsffadul@gmail.com>
2021-09-02 09:16:19 -0300

    Custom validation: unique email when inserting Client

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/dto/validations/ClientInsertValidator.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/dto/validations/ClientInsertValidator.java
@@ -1,18 +1,26 @@
 package com.leonardofadul.springboot.ionic.learning.project.dto.validations;
 
+import com.leonardofadul.springboot.ionic.learning.project.domain.Client;
 import com.leonardofadul.springboot.ionic.learning.project.domain.enums.ClientType;
 import com.leonardofadul.springboot.ionic.learning.project.dto.ClientNewDTO;
 import com.leonardofadul.springboot.ionic.learning.project.dto.validations.utils.BR;
+import com.leonardofadul.springboot.ionic.learning.project.repositories.ClientRepository;
 import com.leonardofadul.springboot.ionic.learning.project.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
     @Override
     public void initialize(ClientInsert ann) {
     }
+
     @Override
     public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
@@ -23,6 +31,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 
         if(objDto.getClientType().equals(ClientType.LEGALPERSON.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())){
             list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ."));
+        }
+
+        Client aux = clientRepository.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "Email already exists."));
         }
 
         for (FieldMessage e : list) {
--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/repositories/ClientRepository.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/repositories/ClientRepository.java
@@ -3,7 +3,11 @@ package com.leonardofadul.springboot.ionic.learning.project.repositories;
 import com.leonardofadul.springboot.ionic.learning.project.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    @Transactional(readOnly = true)
+    Client findByEmail(String email);
 }
