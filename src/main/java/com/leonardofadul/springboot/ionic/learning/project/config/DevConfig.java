3e51c98283c3eff153471c60e8d0a9a792246a40
Leonardo Fadul <lsffadul@gmail.com>
2021-09-03 10:33:03 -0300

    Created dev profile

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/config/DevConfig.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.leonardofadul.springboot.ionic.learning.project.config;
 
 import com.leonardofadul.springboot.ionic.learning.project.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,14 +10,20 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String databaseStrategy;
+
     @Bean
     public boolean instantiateDatabase() throws ParseException {
+        if(!databaseStrategy.equals("create")){
+            return false;
+        }
         dbService.instantiateTestDatabase();
         return true;
     }
