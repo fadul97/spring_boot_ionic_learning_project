8ba6b2869037ee546289b0aa9f92912a8dad8730
Leonardo Fadul <lsffadul@gmail.com>
2021-09-10 10:02:43 -0300

    Authorizing endpoints for specific profiles

--- src/main/java/com/leonardofadul/springboot/ionic/learning/project/resources/CategoryResource.java
+++ src/main/java/com/leonardofadul/springboot/ionic/learning/project/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import com.leonardofadul.springboot.ionic.learning.project.services.CategoryServ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,6 +28,7 @@ public class CategoryResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objDTO){
         Category obj = categoryService.fromDTO(objDTO);
@@ -36,6 +38,7 @@ public class CategoryResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody CategoryDTO objDTO, @PathVariable Integer id){
         Category obj = categoryService.fromDTO(objDTO);
@@ -44,6 +47,7 @@ public class CategoryResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         categoryService.delete(id);
